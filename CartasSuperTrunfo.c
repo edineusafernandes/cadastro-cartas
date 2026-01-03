793a4a3d999f4ee91b71fccff67cba7d470934d4
Edineusa <134114854+edineusafernandes@users.noreply.github.com>
2026-01-02 22:57:44 -0300

    Implement card input and display for two cities
    Atividade realizada em repositório pessoal; correção final com ajuda da IA e colado aqui para cumprir rito avaliativo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,81 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main (){
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    char Estado1, Estado2;
+    char codigo_da_carta1[4], codigo_da_carta2[4];
+    char nome_da_cidade1[30], nome_da_cidade2[30];
+    float area1, area2;
+    double PIB1, PIB2;
+    int populacao1, populacao2, pontos_turisticos1, pontos_turisticos2;
 
-  // Área para entrada de dados
+    printf("CARTA 1\n");
 
-  // Área para exibição dos dados da cidade
+    printf("Digite a letra corresṕondente ao Estado (A a H): \n");
+    scanf(" %c", &Estado1);
+    
+    printf("Digite a letra do Estado seguida de um número de 01 a 04 (ex.: A01, B02): \n");
+    scanf("%s", codigo_da_carta1);
 
-return 0;
-} 
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", nome_da_cidade1);
+
+    printf("Digite a população corresṕondente ao Estado: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a área corresṕondente ao Estado: \n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB corresṕondente ao Estado (A a H): \n");
+    scanf("%lf", &PIB1);
+
+    printf("Digite o número de pontos turísticos corresṕondente ao Estado (A a H): \n");
+    scanf("%d", &pontos_turisticos1);
+
+
+    printf("CARTA 2\n");
+
+    printf("Digite a letra corresṕondente ao Estado (A a H): \n");
+    scanf(" %c", &Estado2);
+    
+    printf("Digite a letra do Estado seguida de um número de 01 a 04 (ex.: A01, B02): \n");
+    scanf("%s", codigo_da_carta2);
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", nome_da_cidade2);
+
+    printf("Digite a população corresṕondente ao Estado: \n");
+    scanf("%d", &populacao2);
+
+    printf("Digite a área corresṕondente ao Estado: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB corresṕondente ao Estado (A a H): \n");
+    scanf("%lf", &PIB2);
+
+    printf("Digite o número de pontos turísticos corresṕondente ao Estado (A a H): \n");
+    scanf("%d", &pontos_turisticos2);
+
+    printf("Carta 1: \n");
+    printf("ESTADO: %c\n", Estado1);
+    printf("Código: %s\n", codigo_da_carta1);
+    printf("Nome da Cidade: %s\n", nome_da_cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Àrea: %f km2\n", area1);
+    printf("PIB: %.2lf bilhões de reais\n", PIB1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+
+    printf("---------------------*---------------------\n");
+
+    printf("Carta 2: \n");
+    printf("ESTADO: %c\n", Estado2);
+    printf("Código: %s\n", codigo_da_carta2);
+    printf("Nome da Cidade: %s\n", nome_da_cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Àrea: %f km2\n", area2);
+    printf("PIB: %.2lf bilhões de reais\n", PIB2);
+    printf("Número de Pontos Turísticos: %d", pontos_turisticos2);
+    
+
+    return 0;
+}
