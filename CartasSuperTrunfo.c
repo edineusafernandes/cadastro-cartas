6f59932a363641f3c32d614de1191532a4ff62e3
Edineusa <134114854+edineusafernandes@users.noreply.github.com>
2026-01-04 02:36:45 +0000

    calculos_adicionados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,17 +5,9 @@ int main (){
     char Estado1, Estado2;
     char codigo_da_carta1[4], codigo_da_carta2[4];
     char nome_da_cidade1[30], nome_da_cidade2[30];
-    float area1, area2;
+    float area1, area2, densi_pop1, densi_pop2, pib_perCapta1, pib_perCapta2;
     double PIB1, PIB2;
     int populacao1, populacao2, pontos_turisticos1, pontos_turisticos2;
-
-    //calculando a densidade populacional de cada cidade
-    densi_pop1 = (float) populacao1/area1;
-    densi_pop2 = (float) populacao2/area2;
-
-    //calculando o PIB per capta
-    pib_perCapta1 = (float) PIB1/populacao1;
-    pib_perCapta2 = (float) PIB2/populacao2;
 
     printf("CARTA 1\n");
 
@@ -34,11 +26,17 @@ int main (){
     printf("Digite a área corresṕondente ao Estado: \n");
     scanf("%f", &area1);
 
-    printf("Digite o PIB corresṕondente ao Estado (A a H): \n");
+    printf("Digite o PIB corresṕondente ao Estado: \n");
     scanf("%lf", &PIB1);
 
-    printf("Digite o número de pontos turísticos corresṕondente ao Estado (A a H): \n");
+    printf("Digite o número de pontos turísticos corresṕondente ao Estado: \n");
     scanf("%d", &pontos_turisticos1);
+
+    //calculando a densidade populacional de cada cidade
+    densi_pop1 = (float) populacao1/area1;
+
+    //calculando o PIB per capta
+    pib_perCapta1 = (float) PIB1/populacao1;
 
 
     printf("CARTA 2\n");
@@ -58,11 +56,17 @@ int main (){
     printf("Digite a área corresṕondente ao Estado: \n");
     scanf("%f", &area2);
 
-    printf("Digite o PIB corresṕondente ao Estado (A a H): \n");
+    printf("Digite o PIB corresṕondente ao Estado: \n");
     scanf("%lf", &PIB2);
 
-    printf("Digite o número de pontos turísticos corresṕondente ao Estado (A a H): \n");
+    printf("Digite o número de pontos turísticos corresṕondente ao Estado: \n");
     scanf("%d", &pontos_turisticos2);
+
+    //calculando a densidade populacional de cada cidade
+    densi_pop2 = (float) populacao2/area2;
+
+    //calculando o PIB per capta
+    pib_perCapta2 = (float) PIB2/populacao2;
 
     printf("Carta 1: \n");
     printf("ESTADO: %c\n", Estado1);
@@ -72,6 +76,8 @@ int main (){
     printf("Àrea: %f km2\n", area1);
     printf("PIB: %.2lf bilhões de reais\n", PIB1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f\n", densi_pop1);
+    printf("PIB per capta: %.2f\n", pib_perCapta1);
 
     printf("---------------------*---------------------\n");
 
@@ -82,7 +88,9 @@ int main (){
     printf("População: %d\n", populacao2);
     printf("Àrea: %f km2\n", area2);
     printf("PIB: %.2lf bilhões de reais\n", PIB2);
-    printf("Número de Pontos Turísticos: %d", pontos_turisticos2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f\n", densi_pop2);
+    printf("PIB per capta: %.2f\n", pib_perCapta2);
     
 
     return 0;
